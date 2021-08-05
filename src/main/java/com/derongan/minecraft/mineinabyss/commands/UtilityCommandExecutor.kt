436e6656cff1efa988aa7f6b6b32048eeb769f10
Norazan <thendjes17@gmail.com>
2021-08-05 20:07:10 +0200

    finish clearcontainers command implementation

--- src/main/java/com/derongan/minecraft/mineinabyss/commands/UtilityCommandExecutor.kt
+++ src/main/java/com/derongan/minecraft/mineinabyss/commands/UtilityCommandExecutor.kt
@@ -12,12 +12,9 @@ import com.mineinabyss.idofront.commands.extensions.actions.playerAction
 import com.mineinabyss.idofront.messaging.info
 import com.okkero.skedule.BukkitSchedulerController
 import com.okkero.skedule.schedule
-import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.block.Container
 import org.bukkit.command.CommandSender
-import kotlin.math.abs
-import kotlin.math.ceil
 
 @ExperimentalCommandDSL
 object UtilityCommandExecutor : IdofrontCommandExecutor() {
@@ -60,29 +57,24 @@ object UtilityCommandExecutor : IdofrontCommandExecutor() {
         val worldsToBeChecked = layer.sections.groupBy { it.world }
         worldsToBeChecked.forEach { (world, sections) ->
             sections.forEach { section ->
-                //sender.info("===== Checking section ${section.name} in world ${world.name} =====")
-
-//                val nrOfChunks =
-//                    ceil((abs(section.region.b.x - section.region.a.x) * abs(section.region.b.z - section.region.a.z)) / 16.0)
-//                var currentChunk = 0
-
-                for (x in section.region.a.x..section.region.b.x+16 step 16) {
-                    for (z in section.region.a.z..section.region.b.z+16 step 16) {
-                        sender.info("x:$x z:$z")
-                        val chunk = Location(world, x.toDouble(), 0.0, z.toDouble()).chunk
+                for (x in (section.region.a.x / 16)..(section.region.b.x / 16)){
+                    for(z in (section.region.a.z / 16)..(section.region.b.z / 16)){
+                        val chunk = world.getChunkAt(x, z)
                         chunk.load()
-                        val containers = chunk.tileEntities.filterIsInstance<Container>()
+                        val containers = chunk.tileEntities
+                            .filterIsInstance<Container>()
+                            .filter { section.region.contains(it.location.x.toInt(), it.location.z.toInt()) }
+
                         containers.forEach { container ->
                             if (container.inventory.contains(item)) {
                                 val numberOfItems = container.inventory.contents
                                     .filter { it != null && it.type == item }
                                     .sumOf { it.amount }
                                 container.inventory.remove(item)
-                                sender.info("Removed $numberOfItems ${item.name} from a container at x:${container.x} y:${container.y} z:${container.z} in layer ${layer.name}")
+                                sender.info("Removed $numberOfItems ${item.name} from a container at x:${container.x} y:${container.y} z:${container.z} in layer ${layer.name} (world ${world.name})")
                             }
                         }
                         chunk.unload()
-                        //currentChunk++
                         yield()
                     }
                 }
