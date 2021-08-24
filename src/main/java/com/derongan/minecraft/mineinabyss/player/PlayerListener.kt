67aada55a7c2861b830c511a8c2c04008f43a186
Boy <sivertpaulsen2@gmail.com>
2021-08-24 16:56:23 +0200

    have crops auto-replant when clicked make fortune actually affect crop drop amount

--- src/main/java/com/derongan/minecraft/mineinabyss/player/PlayerListener.kt
+++ src/main/java/com/derongan/minecraft/mineinabyss/player/PlayerListener.kt
@@ -11,22 +11,33 @@ import com.mineinabyss.idofront.destructure.component2
 import com.mineinabyss.idofront.messaging.color
 import com.mineinabyss.idofront.messaging.error
 import com.mineinabyss.idofront.messaging.logWarn
+import org.bukkit.Bukkit
 import org.bukkit.ChatColor
+import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.attribute.Attribute
+import org.bukkit.block.data.Ageable
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.entity.EntityPotionEffectEvent
 import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerExpChangeEvent
-import org.bukkit.event.player.PlayerItemConsumeEvent
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.inventory.ItemStack
 import org.bukkit.potion.PotionEffectType
+import org.bukkit.util.Vector
 import java.io.IOException
+
 
 object PlayerListener : Listener {
     @EventHandler
@@ -102,5 +113,50 @@ object PlayerListener : Listener {
                 player.error("The ${ChatColor.BOLD}Resistance Effect ${ChatColor.RED}has been disabled")
             }
         }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    fun PlayerInteractEvent.onPlayerHarvest() {
+        val block = clickedBlock
+        var fortuneMultiplier = 0
+
+        if (!hasBlock() || hand != EquipmentSlot.HAND || action != Action.RIGHT_CLICK_BLOCK) return
+
+        val handItem = player.inventory.itemInMainHand
+
+        if (handItem.type != Material.AIR && handItem.itemMeta.enchants.contains(Enchantment.LOOT_BONUS_BLOCKS)) {
+            fortuneMultiplier = handItem.itemMeta.getEnchantLevel(Enchantment.LOOT_BONUS_BLOCKS)
+        }
+
+        val crops: Array<ItemStack> = when (block?.type) {
+            Material.WHEAT -> arrayOf(ItemStack(Material.WHEAT, 1 + (Math.random() * (2 + fortuneMultiplier)).toInt()))
+            Material.CARROTS -> arrayOf(ItemStack(Material.CARROT, 1 + (Math.random() * (2 + fortuneMultiplier)).toInt()))
+            Material.POTATOES -> arrayOf(ItemStack(Material.POTATO, 1 + (Math.random() * (2 + fortuneMultiplier)).toInt()))
+            Material.BEETROOTS -> arrayOf(ItemStack(Material.BEETROOT, 1 + (Math.random() * (2 + fortuneMultiplier)).toInt()))
+            Material.NETHER_WART -> arrayOf(ItemStack(Material.NETHER_WART, 1 + (Math.random() * (2 + fortuneMultiplier)).toInt()))
+            else -> return
+        }
+
+        if(block.blockData !is Ageable) return
+
+        val aging: Ageable = block.blockData as Ageable
+
+        if (aging.age != aging.maximumAge) return
+
+        val breakCrop = BlockBreakEvent(block, player)
+        Bukkit.getPluginManager().callEvent(breakCrop)
+        if (breakCrop.isCancelled) return
+
+        aging.age = 0
+        block.blockData = aging
+
+        player.swingMainHand()
+
+        for (drop in crops) { dropCrop(block.location, drop) }
+    }
+
+    private fun dropCrop(loc: Location, drop: ItemStack) {
+        loc.world.dropItem(loc.add(Vector.getRandom().subtract(Vector(.5, .5, .5)).multiply(0.5)), drop).velocity =
+            Vector.getRandom().add(Vector(-.5, +.5, -.5)).normalize().multiply(.15)
     }
 }
