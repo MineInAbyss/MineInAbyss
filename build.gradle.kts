b667fb24833bee838e3c96d17d658e36ac942656
Scyu_ <scyu@scyu.dev>
2021-10-05 20:23:58 +0100

    Updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
+import Com_mineinabyss_conventions_platform_gradle.Deps
+
 val idofrontVersion: String by project
 
 plugins {
     id("com.mineinabyss.conventions.kotlin")
     id("com.mineinabyss.conventions.papermc")
+    id("com.mineinabyss.conventions.slimjar")
+    id("com.mineinabyss.conventions.copyjar")
     id("com.mineinabyss.conventions.publication")
     kotlin("plugin.serialization")
 }
@@ -17,28 +21,14 @@ dependencies {
     compileOnly("com.mineinabyss:geary-commons-papermc:0.1.2")
     compileOnly("com.mineinabyss:looty:0.3.17")
     compileOnly("com.derongan.minecraft:deeperworld:0.3.58")
-    compileOnly("com.github.MilkBowl:VaultAPI:1.7") { exclude(group ="org.bukkit") }
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7") { exclude(group = "org.bukkit") }
 
-    // From plugin deps
-    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json")
-    compileOnly("com.charleskorn.kaml:kaml")
-    compileOnly("com.github.okkero:skedule")
+    // From Geary
+    slim(Deps.kotlinx.serialization.json)
+    slim(Deps.kotlinx.serialization.kaml)
+    slim(Deps.kotlinx.coroutines)
+    slim(Deps.minecraft.skedule)
 
     // Shaded
-    implementation("com.github.DRE2N:HeadLib:7e2d443678")
-    implementation("com.derongan.minecraft:guiy:0.1.10")
     implementation("com.mineinabyss:idofront:$idofrontVersion")
-}
-
-
-tasks {
-    shadowJar {
-//        minimize()
-//        relocate("com.derongan.minecraft.guiy", "${project.group}.${project.name}.guiy".toLowerCase())
-//        relocate("com.mineinabyss.idofront", "${project.group}.${project.name}.idofront".toLowerCase())
-    }
-
-    build {
-        dependsOn(shadowJar)
-    }
 }
