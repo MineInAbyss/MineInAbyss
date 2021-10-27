9d255351cb9dcffd41eb5b2dc55fdb5acabf2057
Boy <sivertpaulsen2@gmail.com>
2021-10-27 14:07:49 +0200

    tags and enchantment declarations

---
+++ src/main/java/com/derongan/minecraft/mineinabyss/systems/BaneOfKuongatariListener.kt
@@ -0,0 +1,23 @@
+package com.derongan.minecraft.mineinabyss.systems
+
+import com.derongan.minecraft.mineinabyss.components.Insect
+import com.mineinabyss.geary.minecraft.access.toGearyOrNull
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+
+object BaneOfKuongatariListener : Listener {
+
+    @EventHandler
+    fun EntityDamageByEntityEvent.onInsectHit() {
+        val player = damager as? Player ?: return
+        val insect = entity.toGearyOrNull()?.get<Insect>() ?: return
+        val item = player.inventory.itemInMainHand
+        val baneOfKuon = CustomEnchants.BANE_OF_KUONGATARI
+
+        // Ideally this would use getDamageIncrease function
+        if (item.containsEnchantment(baneOfKuon)) damage += item.getEnchantmentLevel(baneOfKuon) * 2
+    }
+
+}
---
+++ src/main/java/com/derongan/minecraft/mineinabyss/systems/BirdSwatterListener.kt
@@ -0,0 +1,23 @@
+package com.derongan.minecraft.mineinabyss.systems
+
+import com.derongan.minecraft.mineinabyss.components.Bird
+import com.mineinabyss.geary.minecraft.access.toGearyOrNull
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+
+object BirdSwatterListener : Listener {
+
+    @EventHandler
+    fun EntityDamageByEntityEvent.onBirdHit() {
+        val player = damager as? Player ?: return
+        val bird = entity.toGearyOrNull()?.get<Bird>() ?: return
+        val item = player.inventory.itemInMainHand
+        val birdSwatter = CustomEnchants.BIRD_SWATTER
+
+        // Ideally this would use getDamageIncrease function
+        if (item.containsEnchantment(birdSwatter)) damage += item.getEnchantmentLevel(birdSwatter) * 2
+    }
+
+}
