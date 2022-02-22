56b749fec82ca41b594b22b1122b8e69ff4739c9
Boy <sivertpaulsen2@gmail.com>
2022-02-22 18:28:33 +0100

    add particle system

--- mineinabyss-components/src/main/kotlin/com/mineinabyss/components/relics/BoundingLance.kt
+++ mineinabyss-components/src/main/kotlin/com/mineinabyss/components/relics/BoundingLance.kt
@@ -2,10 +2,14 @@ package com.mineinabyss.components.relics
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.bukkit.Particle
 import org.bukkit.Sound
 
 @Serializable
 @SerialName("mineinabyss:bounding_lance")
 class BoundingLance(
-    val placeSound: Sound = Sound.BLOCK_CHAIN_PLACE
+    val placeSound: Sound = Sound.BLOCK_CHAIN_PLACE,
+    val effectRadius: Double = 1.5,
+    val effectDuration: Long = 10,
+    val effectParticles: List<Particle> = listOf(Particle.DRIP_LAVA)
 )
--- mineinabyss-features/src/main/kotlin/com/mineinabyss/relics/BoundingLanceListener.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/relics/BoundingLanceListener.kt
@@ -1,12 +1,16 @@
 package com.mineinabyss.relics
 
 import com.mineinabyss.components.relics.BoundingLance
+import com.mineinabyss.geary.papermc.access.toGeary
 import com.mineinabyss.geary.papermc.spawnFromPrefab
 import com.mineinabyss.geary.papermc.toPrefabKey
+import com.mineinabyss.helpers.dropItems
 import com.mineinabyss.idofront.entities.rightClicked
-import com.mineinabyss.idofront.messaging.broadcastVal
 import com.mineinabyss.idofront.util.toMCKey
+import com.mineinabyss.looty.LootyFactory
 import com.mineinabyss.looty.tracking.toGearyOrNull
+import com.mineinabyss.mineinabyss.core.mineInAbyss
+import com.okkero.skedule.schedule
 import org.bukkit.block.BlockFace
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -32,10 +36,40 @@ class BoundingLanceListener : Listener {
             else
                 loc.first()?.location?.toCenterLocation()?.apply { y -= 0.5 } ?: return
 
-        val prefab = "mineinabyss:boundinglance".toMCKey().toPrefabKey()
+        val entityPrefab = "mineinabyss:boundinglance".toMCKey().toPrefabKey()
+        val itemPrefab = "mineinabyss:bounding_lance".toMCKey().toPrefabKey()
 
-        newLoc.spawnFromPrefab(prefab)
-        newLoc.broadcastVal()
+        item.subtract(1)
+        newLoc.spawnFromPrefab(entityPrefab)
         player.playSound(newLoc, boundingLance.placeSound, 1f, 1f)
+
+        mineInAbyss.schedule {
+            clickedBlock!!.location.getNearbyEntities(1.1, 1.1, 1.1).firstOrNull {
+                it.toGeary().has<BoundingLance>()
+            }?.let { entity,  ->
+                val lance = entity.toGeary().get<BoundingLance>() ?: return@schedule
+                var timePassed = 0L
+                val lastTime = System.currentTimeMillis()
+                do {
+                    entity.world.spawnParticle(
+                        lance.effectParticles.random(),
+                        entity.location.toCenterLocation(),
+                        100,
+                        lance.effectRadius,
+                        0.0,
+                        lance.effectRadius,
+                        0.0
+                    )
+                    timePassed += System.currentTimeMillis() - lastTime
+                    waitFor(1)
+                }while (timePassed < lance.effectDuration * 1000)
+                waitFor(60)
+                entity.remove()
+                dropItems(entity.location.toCenterLocation(), LootyFactory.createFromPrefab(itemPrefab)!!)
+            }
+        }
+
+
     }
 }
+
