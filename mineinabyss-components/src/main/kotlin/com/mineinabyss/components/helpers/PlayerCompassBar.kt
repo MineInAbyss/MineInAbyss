ce903a15ffc3b4faf041e255bb1da814b01d2e4c
Boy <sivertpaulsen2@gmail.com>
2022-02-22 19:20:02 +0100

    make bossbar stored on a component

---
+++ mineinabyss-components/src/main/kotlin/com/mineinabyss/components/helpers/PlayerCompassBar.kt
@@ -0,0 +1,12 @@
+package com.mineinabyss.components.helpers
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import net.kyori.adventure.bossbar.BossBar
+import net.kyori.adventure.text.Component
+
+@Serializable
+@SerialName("mineinabyss:player_compass_bar")
+class PlayerCompassBar {
+    val compassBar: BossBar = BossBar.bossBar(Component.text(":arrow_null:"), 1.0f, BossBar.Color.WHITE, BossBar.Overlay.PROGRESS)
+}
--- mineinabyss-features/src/main/kotlin/com/mineinabyss/relics/StarCompassListener.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/relics/StarCompassListener.kt
@@ -1,14 +1,17 @@
 package com.mineinabyss.relics
 
 import com.mineinabyss.components.helpers.HideBossBarCompass
+import com.mineinabyss.components.helpers.PlayerCompassBar
 import com.mineinabyss.components.relics.StarCompass
 import com.mineinabyss.deeperworld.world.section.section
 import com.mineinabyss.geary.autoscan.AutoScan
 import com.mineinabyss.geary.ecs.accessors.TargetScope
 import com.mineinabyss.geary.ecs.accessors.building.get
 import com.mineinabyss.geary.ecs.api.systems.TickingSystem
+import com.mineinabyss.geary.papermc.store.encodeComponentsTo
 import com.mineinabyss.helpers.bossbarCompass
 import com.mineinabyss.idofront.items.editItemMeta
+import com.mineinabyss.idofront.messaging.broadcastVal
 import com.mineinabyss.looty.tracking.toGearyOrNull
 import net.kyori.adventure.bossbar.BossBar
 import net.kyori.adventure.text.Component
@@ -31,6 +34,17 @@ class StarCompassSystem : TickingSystem(interval = 0.1.seconds) {
         val gearyCompass = compass.toGearyOrNull(player) ?: return
         val starCompass = gearyCompass.get<StarCompass>() ?: return
 
+        entity.has<PlayerCompassBar>().broadcastVal()
+        var bar = compassBar
+        if (entity.has<PlayerCompassBar>()) {
+            entity.encodeComponentsTo(player)
+            bar = entity.get<PlayerCompassBar>()?.compassBar!!
+        }
+        else {
+            entity.setPersisting(PlayerCompassBar)
+        }
+
+
         val sectionCenter = player.location.section?.region?.center
 
         if (sectionCenter != null) starCompass.compassLocation = Location(player.world, sectionCenter.x.toDouble(), 0.0, sectionCenter.z.toDouble())
@@ -38,7 +52,7 @@ class StarCompassSystem : TickingSystem(interval = 0.1.seconds) {
         // Let player toggle between having a bossbar-compass or item compass
         if (!gearyCompass.has<HideBossBarCompass>()) {
             compass.type = Material.PAPER
-            player.bossbarCompass(starCompass.compassLocation!!, compassBar)
+            player.bossbarCompass(starCompass.compassLocation!!, bar)
         }
         else {
             compass.type = Material.COMPASS
@@ -46,7 +60,7 @@ class StarCompassSystem : TickingSystem(interval = 0.1.seconds) {
                 lodestone = starCompass.compassLocation
                 isLodestoneTracked = false
             }
-            player.hideBossBar(compassBar)
+            player.hideBossBar(bar)
         }
     }
 }
