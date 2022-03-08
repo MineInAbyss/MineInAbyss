41fab243ae30c154e4e9eeb40c78f840cec1738d
Boy <sivertpaulsen2@gmail.com>
2022-03-08 18:38:49 +0100

    remove compassbar if player drops/looses compass

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/relics/StarCompassListener.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/relics/StarCompassListener.kt
@@ -6,7 +6,6 @@ import com.mineinabyss.components.relics.StarCompass
 import com.mineinabyss.deeperworld.world.section.section
 import com.mineinabyss.geary.autoscan.AutoScan
 import com.mineinabyss.geary.ecs.accessors.TargetScope
-import com.mineinabyss.geary.ecs.accessors.building.get
 import com.mineinabyss.geary.ecs.api.systems.TickingSystem
 import com.mineinabyss.geary.papermc.access.toGeary
 import com.mineinabyss.helpers.bossbarCompass
@@ -20,29 +19,35 @@ import kotlin.time.Duration.Companion.seconds
 
 @AutoScan
 class StarCompassSystem : TickingSystem(interval = 0.1.seconds) {
-    private val TargetScope.player by get<Player>()
-
     override fun TargetScope.tick() {
+        val player = entity.get<Player>() ?: return
         val compass =
-        entity.get<Player>()?.inventory?.contents?.firstOrNull() {
-            if (it == null) return@firstOrNull false
-            it.toGearyOrNull(player)?.has<StarCompass>() ?: return@firstOrNull false
-        } ?: return
+            player.inventory.contents.firstOrNull() {
+                if (it == null) return@firstOrNull false
+                it.toGearyOrNull(player)?.has<StarCompass>() ?: return@firstOrNull false
+            }
+
+        if (compass == null) {
+            if (player.toGeary().has<PlayerCompassBar>()) player.toGeary().remove<PlayerCompassBar>()
+            return
+        }
+
         val starCompass = compass.toGearyOrNull(player)?.get<StarCompass>() ?: return
         val playerBar = player.toGeary().getOrSetPersisting { PlayerCompassBar() }
         val hideCompass = player.toGeary().has<HideBossBarCompass>()
         val sectionCenter = player.location.section?.region?.center
 
-        if (sectionCenter != null) starCompass.compassLocation = Location(player.world, sectionCenter.x.toDouble(), 0.0, sectionCenter.z.toDouble())
+        if (sectionCenter != null) starCompass.compassLocation =
+            Location(player.world, sectionCenter.x.toDouble(), 0.0, sectionCenter.z.toDouble())
 
         // Let player toggle between having a bossbar-compass or item compass
         if (!hideCompass) {
             compass.type = Material.PAPER
             player.bossbarCompass(starCompass.compassLocation!!, playerBar.compassBar)
-        }
-        else {
+        } else {
             compass.type = Material.COMPASS
-            compass.editItemMeta { this as CompassMeta
+            compass.editItemMeta {
+                this as CompassMeta
                 lodestone = starCompass.compassLocation
                 isLodestoneTracked = false
             }
