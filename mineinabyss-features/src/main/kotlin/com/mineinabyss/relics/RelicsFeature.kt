73dadc86c2c709d77b954a8e3ad48ff255b3297c
Boy0000 <62521371+Boy0000@users.noreply.github.com>
2022-03-14 14:11:03 +0100

    Star compass (#137)
    * Star Compass
    
    * remove GearyListener
    
    * small touch-up
    
    * change to ticking system
    
    * option for titlebar starcompass + toggle
    
    * last small tweaks
    
    * make compassToggle use a component
    
    * make compass show :arrow_null: on non-managed sections or different worlds
    
    * fix orientation
    
    * make bossbar stored on a component
    
    * fix component stored on player
    
    * component to toggle compassState
    
    * remove compassbar if player drops/looses compass
    
    * make main-class only iterate over players with relic + new class to clear old bars

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/relics/RelicsFeature.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/relics/RelicsFeature.kt
@@ -1,12 +1,17 @@
 package com.mineinabyss.relics
 
+import com.mineinabyss.components.helpers.HideBossBarCompass
+import com.mineinabyss.geary.papermc.access.toGeary
+import com.mineinabyss.idofront.commands.extensions.actions.playerAction
 import com.mineinabyss.idofront.plugin.registerEvents
 import com.mineinabyss.mineinabyss.core.AbyssFeature
 import com.mineinabyss.mineinabyss.core.MineInAbyssPlugin
+import com.mineinabyss.mineinabyss.core.commands
 import com.mineinabyss.mineinabyss.core.geary
 import com.mineinabyss.relics.sickle.SickleListener
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.bukkit.entity.Player
 
 @Serializable
 @SerialName("relics")
@@ -19,5 +24,23 @@ class RelicsFeature : AbyssFeature {
             )
         }
         registerEvents(SickleListener())
+
+        commands {
+            mineinabyss {
+                "relics"(desc = "Commands for relic-related stuff") {
+                    "star_compass"(desc = "Commands related to the Star Compass") {
+                        "toggle" {
+                            playerAction {
+                                val player = sender as Player
+                                val geary = player.toGeary()
+
+                                if (geary.has<HideBossBarCompass>()) geary.remove<HideBossBarCompass>()
+                                else geary.setPersisting(HideBossBarCompass())
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
---
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/relics/StarCompassListener.kt
@@ -0,0 +1,75 @@
+package com.mineinabyss.relics
+
+import com.mineinabyss.components.helpers.HideBossBarCompass
+import com.mineinabyss.components.helpers.PlayerCompassBar
+import com.mineinabyss.components.relics.StarCompass
+import com.mineinabyss.deeperworld.world.section.section
+import com.mineinabyss.geary.autoscan.AutoScan
+import com.mineinabyss.geary.ecs.accessors.TargetScope
+import com.mineinabyss.geary.ecs.accessors.building.get
+import com.mineinabyss.geary.ecs.api.systems.TickingSystem
+import com.mineinabyss.geary.ecs.entities.parent
+import com.mineinabyss.geary.papermc.access.toGeary
+import com.mineinabyss.helpers.bossbarCompass
+import com.mineinabyss.idofront.items.editItemMeta
+import com.mineinabyss.looty.tracking.toGearyOrNull
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.inventory.meta.CompassMeta
+import kotlin.time.Duration.Companion.seconds
+
+@AutoScan
+class StarCompassSystem : TickingSystem(interval = 0.1.seconds) {
+    private val TargetScope.starCompass by get<StarCompass>()
+
+    override fun TargetScope.tick() {
+        val player = entity.parent?.get<Player>() ?: return
+        val compassList =
+            player.inventory.contents.filter {
+                it != null && it.toGearyOrNull(player)?.has<StarCompass>() == true
+            }
+
+        val playerBar = player.toGeary().getOrSetPersisting { PlayerCompassBar() }
+        val sectionCenter = player.location.section?.region?.center
+
+        if (sectionCenter != null) starCompass.compassLocation =
+            Location(player.world, sectionCenter.x.toDouble(), 0.0, sectionCenter.z.toDouble())
+
+        // Let player toggle between having a bossbar-compass or item compass
+        compassList.forEach { compass ->
+            if (player.toGeary().has<HideBossBarCompass>()) {
+                compass.type = Material.COMPASS
+                compass.editItemMeta {
+                    setCustomModelData(0)
+                    this as CompassMeta
+                    lodestone = starCompass.compassLocation
+                    isLodestoneTracked = false
+                }
+                player.hideBossBar(playerBar.compassBar)
+            } else {
+                compass.editItemMeta {
+                    setCustomModelData(1)
+                    this as CompassMeta
+                    lodestone = null
+                }
+                player.bossbarCompass(starCompass.compassLocation!!, playerBar.compassBar)
+            }
+        }
+    }
+}
+
+/*
+* To not search every players inventory every tick above, it is limited to only those with the item.
+* This class is meant to remove the bar from anyone who might still have it but not the item
+*/
+@AutoScan
+class RemoveStarCompassBar : TickingSystem(interval = 2.seconds) {
+    override fun TargetScope.tick() {
+        val player = entity.get<Player>() ?: return
+        val playerBar = player.toGeary().get<PlayerCompassBar>() ?: return
+
+        player.hideBossBar(playerBar.compassBar)
+        player.toGeary().remove<PlayerCompassBar>()
+    }
+}
