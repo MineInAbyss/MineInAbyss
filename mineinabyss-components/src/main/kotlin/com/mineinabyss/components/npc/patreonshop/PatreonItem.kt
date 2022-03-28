7f3666a5ef3ecfbea26252cf43f7b925a8fc720d
Boy <sivertpaulsen2@gmail.com>
2022-03-28 13:04:19 +0200

    fix bugged items from mobzy-bug

---
+++ mineinabyss-components/src/main/kotlin/com/mineinabyss/components/npc/patreonshop/PatreonItem.kt
@@ -0,0 +1,8 @@
+package com.mineinabyss.components.npc.patreonshop
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+@SerialName("mineinabyss:patreon_item")
+class PatreonItem
--- mineinabyss-features/src/main/kotlin/com/mineinabyss/anticheese/AntiCheeseListener.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/anticheese/AntiCheeseListener.kt
@@ -1,17 +1,23 @@
 package com.mineinabyss.anticheese
 
+import com.mineinabyss.components.npc.patreonshop.PatreonItem
 import com.mineinabyss.helpers.handleCurse
 import com.mineinabyss.idofront.messaging.error
+import com.mineinabyss.looty.tracking.toGearyOrNull
 import com.mineinabyss.mineinabyss.core.layer
 import dev.geco.gsit.api.GSitAPI
 import dev.geco.gsit.api.event.PlayerGetUpSitEvent
 import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockPistonExtendEvent
 import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.entity.EntityPotionEffectEvent
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.ItemStack
 import org.bukkit.potion.PotionEffectType
 
 class AntiCheeseListener: Listener {
@@ -38,6 +44,20 @@ class AntiCheeseListener: Listener {
                 isCancelled = true
                 player.error("${ChatColor.BOLD}Slow Falling ${ChatColor.RED}has been disabled")
             }
+        }
+    }
+
+    // Temporary fix for bugged items dropped by bugged Mobzy
+    @EventHandler
+    fun InventoryClickEvent.removeBuggedWeapons() {
+        val item = currentItem ?: return
+        if (item.type == Material.DIAMOND_SWORD && item.itemMeta?.hasCustomModelData() == true) {
+            if (item.toGearyOrNull(whoClicked as Player)?.has<PatreonItem>() == true) return
+            currentItem = ItemStack(Material.DIAMOND_SWORD)
+        }
+
+        if (item.type == Material.STONE && item.itemMeta.hasEnchant(Enchantment.DEPTH_STRIDER)) {
+            currentItem = ItemStack(Material.STONE)
         }
     }
 }
