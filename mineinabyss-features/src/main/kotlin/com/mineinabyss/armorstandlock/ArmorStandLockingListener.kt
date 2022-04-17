ffa1d472f41338ab64bd6ca9954a7c60c2daf3f5
Christian Bergschneider <37768199+Bloeckchengrafik@users.noreply.github.com>
2022-04-17 12:15:37 +0200

    Really locked armorstands (#146)
    * [really_locked_armorstands] Block Movement through Water or Gravity
    
    * [really_locked_armorstands] Block Movement through Fishing Rods
    
    * [really_locked_armorstands] Block Movement via Pistons. They are as solid as bedrock
    
    * [really_locked_armorstands] Comment/Beautify Code
    
    * [really_locked_armorstands] Hopefully resolve requested changes from Boy0000 and 0ffz
    
    * minor formatting fix
    
    Co-authored-by: Boy <sivertpaulsen2@gmail.com>

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/armorstandlock/ArmorStandLockingListener.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/armorstandlock/ArmorStandLockingListener.kt
@@ -10,6 +10,7 @@ import com.mineinabyss.idofront.spawning.spawn
 import org.bukkit.GameMode
 import org.bukkit.Material
 import org.bukkit.Sound
+import org.bukkit.block.Block
 import org.bukkit.block.BlockFace
 import org.bukkit.entity.ArmorStand
 import org.bukkit.entity.Player
@@ -17,7 +18,10 @@ import org.bukkit.entity.Projectile
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
+import org.bukkit.event.block.BlockPistonExtendEvent
+import org.bukkit.event.block.BlockPistonRetractEvent
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.player.PlayerFishEvent
 import org.bukkit.event.player.PlayerInteractAtEntityEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.EquipmentSlot
@@ -36,10 +40,8 @@ class ArmorStandLockingListener : Listener {
 
         // Wacky code to mimic vanilla placing
         val newLoc =
-            if (blockFace == BlockFace.UP)
-                loc.last()?.location?.toCenterLocation()?.apply { y += 0.5 } ?: return
-            else
-                loc.first()?.location?.toCenterLocation()?.apply { y -= 0.5 } ?: return
+            if (blockFace == BlockFace.UP) loc.last()?.location?.toCenterLocation()?.apply { y += 0.5 } ?: return
+            else loc.first()?.location?.toCenterLocation()?.apply { y -= 0.5 } ?: return
 
         newLoc.spawn<ArmorStand>()?.apply {
             setRotation(player.location.yaw - 180, 0.0F)
@@ -74,21 +76,68 @@ class ArmorStandLockingListener : Listener {
 
         if (!armorStand.lockState) return
 
-        val attacker: Player =
-            when (damager) {
-                is Projectile -> {
-                    (damager as Projectile).shooter as? Player ?: return
-                }
-                is Player -> {
-                    (damager as Player)
-                }
-                else -> {
-                    return
-                }
-            }
+        val attacker: Player = when (damager) {
+            is Projectile -> (damager as Projectile).shooter as? Player ?: return
+            is Player -> (damager as Player)
+            else -> return
+        }
 
         if (!armorStand.isAllowed(attacker.uniqueId) && !attacker.hasPermission("mineinabyss.lockarmorstand.bypass")) {
             attacker.error("You do not have access to interacting with this armor stand!")
+            isCancelled = true
+        }
+    }
+
+    @EventHandler
+    fun PlayerFishEvent.onFishArmorstand() {
+        if (state != PlayerFishEvent.State.CAUGHT_ENTITY) return
+
+        val armorStandBukkit = caught as? ArmorStand ?: return
+        val armorStandGeary = armorStandBukkit.toGeary().get<LockArmorStand>() ?: return
+
+        if (!armorStandGeary.lockState) return
+
+        if (!armorStandGeary.isAllowed(player.uniqueId) && !player.hasPermission("mineinabyss.lockarmorstand.bypass")) {
+            player.error("You do not have access to fishing this armor stand!")
+            hook.remove()
+            isCancelled = true
+        }
+    }
+
+    /**
+     * This function is used to prevent pistons from extending armor stands,
+     * to prevent pistons from pushing blocks into armor stands,
+     * and to prevent pistons from pushing armor stands using honey blocks below them.
+     */
+    private fun onPistonExtendRetract(block: Block, direction: BlockFace, blocks: List<Block>): Boolean {
+        val blocksThePistonChanges =
+            (blocks + block).map { it.location.add(direction.direction).block } + blocks.filter { it.type == Material.HONEY_BLOCK }
+                .map { it.location.add(BlockFace.UP.direction).block }
+
+        for (blockinblocks in blocksThePistonChanges) {
+            // Get all armor stands in the blocks the piston changes
+            val armorStands = blockinblocks.location.getNearbyEntitiesByType(ArmorStand::class.java, 1.0)
+            for (armorStand in armorStands) {
+                // If any armorstand is locked, cancel the event
+                val gearyArmorStand = armorStand.toGeary().get<LockArmorStand>() ?: continue
+                if (!gearyArmorStand.lockState) continue
+
+                return true
+            }
+        }
+        return false
+    }
+
+    @EventHandler
+    fun BlockPistonExtendEvent.onPistonExtend() {
+        if (onPistonExtendRetract(block, direction, blocks)) {
+            isCancelled = true
+        }
+    }
+
+    @EventHandler
+    fun BlockPistonRetractEvent.onPistonRetract() {
+        if (onPistonExtendRetract(block, direction.oppositeFace, blocks)) {
             isCancelled = true
         }
     }
