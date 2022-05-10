d960f92685fb75a92d647803e672c63da369cb18
Boy <sivertpaulsen2@gmail.com>
2022-05-10 15:30:48 +0200

    remove broadcast

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/patreons/PatreonListener.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/patreons/PatreonListener.kt
@@ -3,7 +3,6 @@ package com.mineinabyss.patreons
 import com.mineinabyss.components.players.Patreon
 import com.mineinabyss.geary.papermc.access.toGeary
 import com.mineinabyss.helpers.getGroups
-import com.mineinabyss.idofront.messaging.broadcast
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -13,7 +12,7 @@ class PatreonListener : Listener {
     @EventHandler
     fun PlayerJoinEvent.addPatreonComponent() {
         val isPatreon = player.getGroups().contains("patreon")
-        broadcast(player.toGeary().has<Patreon>())
+
         if (!isPatreon) return
         if (!player.toGeary().has<Patreon>()) player.toGeary().getOrSetPersisting { Patreon() }
         val patreon = player.toGeary().get<Patreon>() ?: return
