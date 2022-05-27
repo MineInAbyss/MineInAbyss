98adb4224c52649878378b2860ea42059d9f481e
Boy <sivertpaulsen2@gmail.com>
2022-05-27 23:02:16 +0200

    modify frost aspect with new api features

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/enchants/enchantments/FrostAspectListener.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/enchants/enchantments/FrostAspectListener.kt
@@ -2,7 +2,6 @@ package com.mineinabyss.enchants.enchantments
 
 import com.github.shynixn.mccoroutine.bukkit.launch
 import com.mineinabyss.enchants.CustomEnchants
-import com.mineinabyss.idofront.time.ticks
 import com.mineinabyss.mineinabyss.core.mineInAbyss
 import kotlinx.coroutines.delay
 import org.bukkit.entity.LivingEntity
@@ -13,37 +12,23 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent
 import kotlin.time.Duration.Companion.seconds
 
 class FrostAspectListener : Listener {
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     fun EntityDamageByEntityEvent.onFrostAspectHit() {
-        val damager = damager as? Player ?: return
-        val item = damager.inventory.itemInMainHand
-        val enchantLvl = item.getEnchantmentLevel(CustomEnchants.FROST_ASPECT)
-        val length = 10.seconds.times(enchantLvl) // Multiplied duration by level.
-        val damageTimer = 10.seconds
-        // TODO use absolute time instead of delta (set length to the exact time the effect finishes)
-        var timePassed = 0L
-        var freezeTimePassed = 0L
-        val lastTime = System.currentTimeMillis()
+        val item = (damager as? Player ?: return).inventory.itemInMainHand
+        val totalFreezeTicks = 5 * item.getEnchantmentLevel(CustomEnchants.FROST_ASPECT)
+        var freezeTicks = 0
 
         if (item.containsEnchantment(CustomEnchants.FROST_ASPECT)) {
+            entity.freezeTicks = entity.maxFreezeTicks
+            entity.lockFreezeTicks(true)
+
             mineInAbyss.launch {
                 do {
-                    val lastFreezeTime = System.currentTimeMillis()
-                    do {
-                        entity.freezeTicks = entity.maxFreezeTicks
-                        freezeTimePassed += System.currentTimeMillis() - lastFreezeTime
-                        delay(1.ticks)
-                    } while ((freezeTimePassed < damageTimer.inWholeMilliseconds) &&
-                        ((entity.freezeTicks == entity.maxFreezeTicks) ||
-                                (entity.freezeTicks in (1 until entity.maxFreezeTicks)))
-                    )
-
-                    freezeTimePassed = 0L
+                    delay(1.seconds)
                     (entity as LivingEntity).damage(1.0)
-                    entity.freezeTicks = entity.maxFreezeTicks
-                    timePassed += System.currentTimeMillis() - lastTime
-                    if ((entity as LivingEntity).health == 0.0) timePassed = length.inWholeMilliseconds
-                } while (timePassed < length.inWholeMilliseconds)
+                    freezeTicks += 1
+                } while (freezeTicks <= totalFreezeTicks)
+                entity.lockFreezeTicks(false)
             }
         }
     }
