304be56ce17f781230c19ff43a10adfcdacb181b
Boy0000 <62521371+Boy0000@users.noreply.github.com>
2022-06-07 20:30:19 +0200

    Merge pull request #160
    * utilize BossBarSerializer

--- mineinabyss-components/src/main/kotlin/com/mineinabyss/components/helpers/PlayerCompassBar.kt
+++ mineinabyss-components/src/main/kotlin/com/mineinabyss/components/helpers/PlayerCompassBar.kt
@@ -1,5 +1,6 @@
 package com.mineinabyss.components.helpers
 
+import com.mineinabyss.idofront.serialization.BossBarSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import net.kyori.adventure.bossbar.BossBar
@@ -8,5 +9,6 @@ import net.kyori.adventure.text.Component
 @Serializable
 @SerialName("mineinabyss:player_compass_bar")
 class PlayerCompassBar {
-    var compassBar: BossBar = BossBar.bossBar(Component.text(":arrow_null:"), 1.0f, BossBar.Color.WHITE, BossBar.Overlay.PROGRESS)
+    var compassBar: @Serializable(with = BossBarSerializer::class) BossBar =
+        BossBar.bossBar(Component.text(":arrow_null:"), 1.0f, BossBar.Color.WHITE, BossBar.Overlay.PROGRESS)
 }
