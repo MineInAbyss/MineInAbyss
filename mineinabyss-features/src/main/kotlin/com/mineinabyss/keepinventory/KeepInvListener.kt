a2747053f101bfcfbc69a15abda9b4e355cabe78
Boy <sivertpaulsen2@gmail.com>
2022-10-24 12:41:59 +0200

    fix keepinv, Closes #180

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/keepinventory/KeepInvListener.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/keepinventory/KeepInvListener.kt
@@ -1,12 +1,21 @@
 package com.mineinabyss.keepinventory
 
 import com.mineinabyss.components.playerData
+import org.bukkit.GameRule
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.world.WorldLoadEvent
 
 class KeepInvListener(private val feature: KeepInvFeature) : Listener {
+
+    // Force keepinv to be false
+    @EventHandler
+    fun WorldLoadEvent.onWorldLoad() {
+        world.setGameRule(GameRule.KEEP_INVENTORY, false)
+    }
+
     @EventHandler
     fun PlayerDeathEvent.optionalKeepInventory() {
         if ((feature.KeepInvInVoid && player.lastDamageCause?.cause == EntityDamageEvent.DamageCause.VOID) ||
@@ -15,6 +24,6 @@ class KeepInvListener(private val feature: KeepInvFeature) : Listener {
             keepLevel = true
             droppedExp = 0
             drops.clear()
-        } else keepInventory = false
+        }
     }
 }
