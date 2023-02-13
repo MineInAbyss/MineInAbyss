1eb7430be68934f63ed2c16e918db89339c07668
Boy <sivertpaulsen2@gmail.com>
2023-02-13 22:06:51 +0100

    forcefully check gamemode for helpermode on join

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/moderation/HelperListener.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/moderation/HelperListener.kt
@@ -1,10 +1,12 @@
 package com.mineinabyss.moderation
 
 import com.mineinabyss.components.moderation.isInHelperMode
+import org.bukkit.GameMode
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerAttemptPickupItemEvent
 import org.bukkit.event.player.PlayerDropItemEvent
+import org.bukkit.event.player.PlayerJoinEvent
 
 class HelperListener : Listener {
 
@@ -16,5 +18,10 @@ class HelperListener : Listener {
     @EventHandler
     fun PlayerDropItemEvent.onDrop() {
         if (player.isInHelperMode) isCancelled = true
+    }
+
+    @EventHandler
+    fun PlayerJoinEvent.onJoin() {
+        if (player.isInHelperMode) player.gameMode = GameMode.SPECTATOR
     }
 }
