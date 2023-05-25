d6a89566d32f8f440eb53b9eaa5d9d6a39e43389
Boy <sivertpaulsen2@gmail.com>
2023-05-25 11:16:47 +0200

    switch back to packet based as method does not work

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/core/CoreFeature.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/core/CoreFeature.kt
@@ -1,16 +1,18 @@
 package com.mineinabyss.features.core
 
-import com.mineinabyss.idofront.plugin.listeners
+import com.comphenix.protocol.ProtocolLibrary
 import com.mineinabyss.mineinabyss.core.AbyssFeature
 import com.mineinabyss.mineinabyss.core.MineInAbyssPlugin
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.bukkit.Bukkit
 
 @Serializable
 @SerialName("core")
 class CoreFeature : AbyssFeature {
 
     override fun MineInAbyssPlugin.enableFeature() {
-        listeners(CoreListener())
+        if (Bukkit.getPluginManager().isPluginEnabled("ProtocolLib"))
+            ProtocolLibrary.getProtocolManager().addPacketListener(CorePacket())
     }
 }
