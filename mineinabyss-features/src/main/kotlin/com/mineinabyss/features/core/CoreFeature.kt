f73286e3574c82f68d9114c60472c6727638afcd
Boy <sivertpaulsen2@gmail.com>
2023-08-08 12:35:02 +0200

    prevent "moved wrongly" from being logged

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/core/CoreFeature.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/core/CoreFeature.kt
@@ -1,21 +1,21 @@
 package com.mineinabyss.features.core
 
 import com.comphenix.protocol.ProtocolLibrary
+import com.mineinabyss.idofront.plugin.Plugins
 import com.mineinabyss.idofront.plugin.listeners
 import com.mineinabyss.mineinabyss.core.AbyssFeature
 import com.mineinabyss.mineinabyss.core.MineInAbyssPlugin
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import org.bukkit.Bukkit
 
 @Serializable
 @SerialName("core")
 class CoreFeature : AbyssFeature {
 
     override fun MineInAbyssPlugin.enableFeature() {
-        if (Bukkit.getPluginManager().isPluginEnabled("ProtocolLib"))
+        if (Plugins.isEnabled("ProtocolLib"))
             ProtocolLibrary.getProtocolManager().addPacketListener(ChestGuiPacket())
 
-        listeners(PreventSignEditListener())
+        listeners(CoreListener(), PreventSignEditListener())
     }
 }
