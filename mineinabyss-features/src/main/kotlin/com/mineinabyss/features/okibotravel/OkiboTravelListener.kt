1f7492e872ab515605d825ff95b2f60d669eea08
Boy <sivertpaulsen2@gmail.com>
2023-08-13 21:05:06 +0200

    fix map interaction and spawn cart to destination

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/okibotravel/OkiboTravelListener.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/okibotravel/OkiboTravelListener.kt
@@ -3,8 +3,10 @@ package com.mineinabyss.features.okibotravel
 import com.mineinabyss.components.okibotravel.OkiboLineStation
 import com.mineinabyss.components.okibotravel.OkiboTraveler
 import com.mineinabyss.features.okibotravel.menu.OkiboMainScreen
+import com.mineinabyss.features.okibotravel.menu.spawnOkiboCart
 import com.mineinabyss.geary.papermc.tracking.entities.toGearyOrNull
 import com.mineinabyss.guiy.inventory.guiy
+import com.mineinabyss.idofront.messaging.error
 import com.mineinabyss.idofront.textcomponents.miniMsg
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -21,8 +23,9 @@ class OkiboTravelListener(private val feature: OkiboTravelFeature) : Listener {
 
     @EventHandler
     fun PlayerInteractEntityEvent.onClickMap() {
-        val station = rightClicked.toGearyOrNull()?.get<OkiboLineStation>()?.name ?: return
-        player.sendMessage("You clicked the map at station ${station}!".miniMsg())
-        //rightClicked.remove()
+        val destination = rightClicked.toGearyOrNull()?.get<OkiboLineStation>() ?: return
+        player.sendMessage("You clicked the map at station ${destination.name}!".miniMsg())
+        val playerStation = okiboLine.config.okiboStations.filter { it != destination }.minByOrNull { it.location.distanceSquared(player.location) } ?: return player.error("You are not near a station!")
+        spawnOkiboCart(player, playerStation, destination)
     }
 }
