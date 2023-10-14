46ae6dd46e13bb8ae14f0ac82af9468d9094f74a
Boy <sivertpaulsen2@gmail.com>
2023-10-14 13:23:12 +0200

    fix: make MusicFeature serializable so it can be enabled

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/music/MusicFeature.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/music/MusicFeature.kt
@@ -6,8 +6,14 @@ import com.mineinabyss.mineinabyss.core.AbyssFeature
 import com.mineinabyss.mineinabyss.core.Configurable
 import com.mineinabyss.mineinabyss.core.MineInAbyssPlugin
 import com.mineinabyss.mineinabyss.core.abyss
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
+@Serializable
+@SerialName("music")
 class MusicFeature : AbyssFeature, Configurable<MusicConfig> {
+    @Transient
     override val configManager = config("music", abyss.dataPath, MusicConfig())
 
     override fun MineInAbyssPlugin.enableFeature() {
