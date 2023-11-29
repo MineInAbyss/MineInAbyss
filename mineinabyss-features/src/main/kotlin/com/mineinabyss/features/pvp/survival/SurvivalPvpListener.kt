a11a4c99cc4423b97bbe1df0c5b6ec6d39dcf42f
Danielle Voznyy <dan.voznyy@gmail.com>
2023-11-28 21:35:23 -0500

    Only show pvp enabled message when from section doesn't have it on

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/pvp/survival/SurvivalPvpListener.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/pvp/survival/SurvivalPvpListener.kt
@@ -2,15 +2,15 @@ package com.mineinabyss.features.pvp.survival
 
 import com.mineinabyss.components.playerData
 import com.mineinabyss.deeperworld.event.PlayerDescendEvent
-import com.mineinabyss.idofront.messaging.error
 import com.mineinabyss.features.helpers.layer
+import com.mineinabyss.idofront.messaging.error
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
 class SurvivalPvpListener : Listener {
     @EventHandler
     fun PlayerDescendEvent.onEnterPvPLayer() {
-        if (toSection.layer?.hasPvpDefault == true && !player.playerData.pvpStatus) {
+        if (fromSection.layer?.hasPvpDefault == false && toSection.layer?.hasPvpDefault == true && !player.playerData.pvpStatus) {
             player.error("PVP is always enabled below this point.")
         }
     }
