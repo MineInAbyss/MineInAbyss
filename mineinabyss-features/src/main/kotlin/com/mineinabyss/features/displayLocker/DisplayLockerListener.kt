5f2de24651267abaa54b235fb9ba89fd2957fe3a
Boy <sivertpaulsen2@gmail.com>
2024-01-04 01:16:57 +0100

    fix: DisplayLocker not working due to geary not tracking yet

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/displayLocker/DisplayLockerListener.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/displayLocker/DisplayLockerListener.kt
@@ -1,14 +1,19 @@
 package com.mineinabyss.features.displayLocker
 
+import com.destroystokyo.paper.event.entity.EntityAddToWorldEvent
+import com.github.shynixn.mccoroutine.bukkit.launch
 import com.mineinabyss.components.displaylocker.LockDisplayItem
 import com.mineinabyss.components.displaylocker.lockedDisplay
 import com.mineinabyss.components.playerData
+import com.mineinabyss.features.abyss
 import com.mineinabyss.geary.papermc.datastore.encodeComponentsTo
 import com.mineinabyss.geary.papermc.tracking.entities.toGeary
 import com.mineinabyss.idofront.messaging.error
 import com.mineinabyss.idofront.messaging.success
 import com.mineinabyss.idofront.spawning.spawn
 import io.papermc.paper.event.player.PlayerItemFrameChangeEvent
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.yield
 import org.bukkit.GameMode
 import org.bukkit.Material
 import org.bukkit.Sound
@@ -16,6 +21,7 @@ import org.bukkit.block.Block
 import org.bukkit.block.BlockFace
 import org.bukkit.entity.*
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.block.BlockPistonExtendEvent
@@ -23,6 +29,7 @@ import org.bukkit.event.block.BlockPistonRetractEvent
 import org.bukkit.event.entity.EntityDamageByBlockEvent
 import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.entity.EntityPlaceEvent
 import org.bukkit.event.entity.ProjectileHitEvent
 import org.bukkit.event.hanging.HangingBreakByEntityEvent
 import org.bukkit.event.hanging.HangingBreakEvent
@@ -39,44 +46,31 @@ class DisplayLockerListener : Listener {
     fun HangingPlaceEvent.onPlaceItemFrame() {
         val (frame, player) = (entity as? ItemFrame ?: return) to (player ?: return)
         val lockState = player.playerData.defaultDisplayLockState
-        frame.toGeary()
-            .getOrSetPersisting { LockDisplayItem(player.uniqueId, lockState, mutableSetOf(player.uniqueId)) }
-        frame.toGeary().encodeComponentsTo(frame)
-        player.playerData.recentInteractEntity = frame.uniqueId
-        when (lockState) {
-            true -> player.success("This Item Frame is now protected!")
-            false -> player.error("Use <b>/mia lock toggle</b> to protect this Item Frame.")
+        abyss.plugin.launch {
+            yield()
+            frame.toGeary().getOrSetPersisting { LockDisplayItem(player.uniqueId, lockState, mutableSetOf(player.uniqueId)) }
+            frame.toGeary().encodeComponentsTo(frame)
+            player.playerData.recentInteractEntity = frame.uniqueId
+            when (lockState) {
+                true -> player.success("This ${frame.name} is now protected!")
+                false -> player.error("Use <b>/mia lock toggle</b> to protect this ${frame.name}.")
+            }
         }
     }
 
-    @EventHandler
-    fun PlayerInteractEvent.onPlaceArmorStand() { // pretty much the same code as before
-        if (player.inventory.itemInMainHand.type != Material.ARMOR_STAND) return
-        if (hand != EquipmentSlot.HAND || blockFace == BlockFace.DOWN || action != Action.RIGHT_CLICK_BLOCK) return
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    fun EntityPlaceEvent.onPlaceArmorStand() {
+        val (entity, player) = (entity as? ArmorStand ?: return) to (player ?: return)
         val lockState = player.playerData.defaultDisplayLockState
-        isCancelled = true
-
-        val initialLocation = player.getLastTwoTargetBlocks(null, 6)
-        val newLocation = initialLocation.last()?.location?.toCenterLocation()?.apply {
-            when (blockFace) {
-                BlockFace.UP -> y += 0.5
-                else -> y -= 0.5
+        abyss.plugin.launch {
+            yield()
+            entity.toGeary().getOrSetPersisting { LockDisplayItem(player.uniqueId, lockState, mutableSetOf(player.uniqueId)) }
+            entity.toGeary().encodeComponentsTo(entity)
+            player.playerData.recentInteractEntity = entity.uniqueId
+            when (lockState) {
+                true -> player.success("This ${entity.name} is now protected!")
+                false -> player.error("Use <b>/mia lock toggle</b> to protect this ${entity.name}.")
             }
-        } ?: return
-
-        newLocation.spawn<ArmorStand>()?.apply {
-            setRotation(player.location.yaw - 180, 0.0F)
-            toGeary().getOrSetPersisting { LockDisplayItem(player.uniqueId, lockState, mutableSetOf(player.uniqueId)) }
-            toGeary().encodeComponentsTo(this)
-            player.playerData.recentInteractEntity = uniqueId
-        } ?: return
-
-        if (player.gameMode != GameMode.CREATIVE) player.inventory.itemInMainHand.subtract()
-        player.playSound(newLocation, Sound.ENTITY_ARMOR_STAND_PLACE, 1f, 1f)
-
-        when (lockState) {
-            true -> player.success("This Armor Stand is now protected!")
-            false -> player.error("Use <b>/mia lock toggle</b> to protect this Armor Stand.")
         }
     }
 
