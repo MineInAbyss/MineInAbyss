1f6909e196e4111bac11c6a917fac6bc5e8e4ed9
Danielle Voznyy <dan.voznyy@gmail.com>
2024-01-15 23:03:16 -0500

    Remove pins system

--- mineinabyss-components/src/main/kotlin/com/mineinabyss/components/pins/AbyssalPin.kt
+++
@@ -1,9 +0,0 @@
-package com.mineinabyss.components.pins
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
-@SerialName("mineinabyss:abyssal_pin")
-class AbyssalPin {
-}
