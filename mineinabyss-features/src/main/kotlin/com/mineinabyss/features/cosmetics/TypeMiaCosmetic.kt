23fad1c8a19d0e7743c7373081f2fbcb529a6144
Boy <sivertpaulsen2@gmail.com>
2024-02-27 18:06:24 +0100

    feat: right click to dye, left to equip/unequip

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/cosmetics/TypeMiaCosmetic.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/cosmetics/TypeMiaCosmetic.kt
@@ -1,6 +1,7 @@
 package com.mineinabyss.features.cosmetics
 
 import com.hibiscusmc.hmccosmetics.cosmetic.Cosmetics
+import com.hibiscusmc.hmccosmetics.gui.special.DyeMenu
 import com.hibiscusmc.hmccosmetics.gui.type.types.TypeCosmetic
 import com.hibiscusmc.hmccosmetics.user.CosmeticUser
 import com.mineinabyss.components.playerData
@@ -22,26 +23,38 @@ class TypeMiaCosmetic : TypeCosmetic("mia_cosmetic") {
         val cost = config.node("cost").int
         val data = player.playerData
         val cosmetic = Cosmetics.getCosmetic(config.node("cosmetic").string) ?: return
-        if (cosmetic == user.getCosmetic(cosmetic.slot) || !cosmetic.requiresPermission() || player.hasPermission(cosmetic.permission)) return super.run(user, config, clickType)
+        val currentCosmetic = user.getCosmetic(cosmetic.slot)
 
         when (clickType) {
-            ClickType.RIGHT -> when (currency) {
-                "mitty_token" -> {
-                    if (data.mittyTokensHeld < cost) return player.error("You don't have enough Mitty Tokens!")
-                    data.mittyTokensHeld -= cost
-                    player.success("You have purchased the cosmetic for $cost Mitty Tokens!")
-                }
-                else -> {
-                    if (data.orthCoinsHeld < cost) return player.error("You don't have enough Orth Coins!")
-                    data.orthCoinsHeld -= cost
-                    player.success("You have purchased the cosmetic for $cost Orth Coins!")
-                }
-            }.apply {
-                luckPerms.userManager.modifyUser(player.uniqueId) {
-                    it.data().add(Node.builder(cosmetic.permission).build())
-                }
-                super.run(user, config, clickType)
+            ClickType.LEFT -> {
+                if (!cosmetic.requiresPermission() || player.hasPermission(cosmetic.permission))
+                    if (cosmetic != currentCosmetic) user.addPlayerCosmetic(cosmetic)
+                    else user.removeCosmeticSlot(cosmetic.slot)
             }
+
+            ClickType.RIGHT ->
+                if (cosmetic == currentCosmetic || !cosmetic.requiresPermission() || player.hasPermission(cosmetic.permission)) {
+                    if (cosmetic.isDyable) DyeMenu.openMenu(user, cosmetic)
+                    else return super.run(user, config, clickType)
+                } else when (currency) {
+                    "mitty_token" -> {
+                        if (data.mittyTokensHeld < cost) return player.error("You don't have enough Mitty Tokens!")
+                        data.mittyTokensHeld -= cost
+                        player.success("You have purchased the cosmetic for $cost Mitty Tokens!")
+                    }
+
+                    else -> {
+                        if (data.orthCoinsHeld < cost) return player.error("You don't have enough Orth Coins!")
+                        data.orthCoinsHeld -= cost
+                        player.success("You have purchased the cosmetic for $cost Orth Coins!")
+                    }
+                }.apply {
+                    luckPerms.userManager.modifyUser(player.uniqueId) {
+                        it.data().add(Node.builder(cosmetic.permission).build())
+                    }
+                    super.run(user, config, clickType)
+                }
+
             else -> super.run(user, config, clickType)
         }
     }
