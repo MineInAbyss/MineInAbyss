ececc865696ec3e605bdb78c18b255fd9e1856b1
Boy <sivertpaulsen2@gmail.com>
2024-08-08 20:23:17 +0200

    fix: add cooldown to okibo-map listener

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/okibotravel/OkiboTravelListener.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/okibotravel/OkiboTravelListener.kt
@@ -8,7 +8,11 @@ import com.mineinabyss.components.playerData
 import com.mineinabyss.features.abyss
 import com.mineinabyss.features.helpers.di.Features.okiboLine
 import com.mineinabyss.features.hubstorage.isInHub
+import com.mineinabyss.geary.actions.ActionGroupContext
+import com.mineinabyss.geary.actions.execute
 import com.mineinabyss.geary.helpers.with
+import com.mineinabyss.geary.papermc.features.common.cooldowns.Cooldowns
+import com.mineinabyss.geary.papermc.features.common.cooldowns.StartCooldown
 import com.mineinabyss.geary.papermc.tracking.entities.toGeary
 import com.mineinabyss.geary.papermc.tracking.entities.toGearyOrNull
 import com.mineinabyss.geary.prefabs.PrefabKey
@@ -23,14 +27,18 @@ import kotlin.time.Duration.Companion.seconds
 
 class OkiboTravelListener : Listener {
 
+    private val okiboMapCooldown = StartCooldown(1.seconds, null, "mineinabyss:okibomap")
+
     @EventHandler
     fun PlayerUseUnknownEntityEvent.onInteractMap() {
-        if (hand != EquipmentSlot.HAND) return
+        val gearyPlayer = player.toGeary().takeIf { hand == EquipmentSlot.HAND } ?: return
+        if (!Cooldowns.isComplete(gearyPlayer, okiboMapCooldown.id)) return
+        okiboMapCooldown.execute(ActionGroupContext(gearyPlayer))
         val destination = getHitboxStation(entityId)?.getStation ?: return
         val playerStation = okiboLine.config.allStations.filter { it != destination }.minByOrNull { it.location.distanceSquared(player.location) } ?: return player.error("You are not near a station!")
         val cost = playerStation.costTo(destination) ?: return player.error("You cannot travel to that station!")
 
-        player.toGeary().with { traveler: OkiboTraveler ->
+        gearyPlayer.with { traveler: OkiboTraveler ->
             when (traveler.selectedDestination) {
                 destination -> {
                     when {
@@ -50,11 +58,11 @@ class OkiboTravelListener : Listener {
         player.info("<gold>You selected <yellow>${destination.name}</yellow> station!")
         player.info("<gold>The cost to travel there will be <yellow>$cost</yellow> Orth Coins.")
         player.info("<gold>Click the map again to confirm your selection.")
-        player.toGeary().set(OkiboTraveler(destination))
+        gearyPlayer.set(OkiboTraveler(destination))
 
         abyss.plugin.launch {
             delay(5.seconds)
-            if (player.isOnline) player.toGeary().remove<OkiboTraveler>()
+            if (player.isOnline) gearyPlayer.remove<OkiboTraveler>()
         }
     }
 
