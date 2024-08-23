f779a7c0da9e0a3d3256f250ee7f3e591d9da990
Boy <sivertpaulsen2@gmail.com>
2024-08-24 01:13:23 +0200

    chore: add GuideBookPage#openMerchantMenu

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/guidebook/GuideBookHelpers.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/guidebook/GuideBookHelpers.kt
@@ -1,4 +1,22 @@
 package com.mineinabyss.features.guidebook
 
-class GuideBookHelpers {
+import com.mineinabyss.features.helpers.TitleItem
+import com.mineinabyss.idofront.serialization.SerializableItemStack
+import net.minecraft.core.component.DataComponentPredicate
+import net.minecraft.core.component.DataComponents
+import net.minecraft.util.Unit
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.trading.ItemCost
+import net.minecraft.world.item.trading.MerchantOffer
+
+object GuideBookHelpers {
+
+    fun MerchantOffer(buyItem: SerializableItemStack): MerchantOffer {
+        return MerchantOffer(ItemStack.fromBukkitCopy(buyItem.toItemStack()))
+    }
+
+    fun MerchantOffer(buyItem: ItemStack, sellItemStack: ItemStack = ItemStack.fromBukkitCopy(TitleItem.transparentItem)): MerchantOffer {
+        buyItem.set(DataComponents.HIDE_TOOLTIP, Unit.INSTANCE)
+        return MerchantOffer(ItemCost(buyItem.itemHolder, 1, DataComponentPredicate.allOf(buyItem.components)), sellItemStack, 1, 1, 1f)
+    }
 }
--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/guidebook/GuideBookPage.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/guidebook/GuideBookPage.kt
@@ -1,10 +1,25 @@
 package com.mineinabyss.features.guidebook
 
+import com.mineinabyss.features.helpers.TitleItem
 import com.mineinabyss.geary.serialization.serializers.InnerSerializer
+import com.mineinabyss.idofront.entities.title
+import com.mineinabyss.idofront.items.editItemMeta
+import com.mineinabyss.idofront.nms.aliases.toNMS
 import com.mineinabyss.idofront.serialization.SerializableItemStack
+import com.mineinabyss.idofront.textcomponents.miniMsg
+import io.papermc.paper.adventure.PaperAdventure
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.MapSerializer
 import kotlinx.serialization.builtins.serializer
+import net.minecraft.core.component.DataComponentPredicate
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.entity.npc.ClientSideMerchant
+import net.minecraft.world.inventory.MerchantMenu
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.trading.ItemCost
+import net.minecraft.world.item.trading.MerchantOffer
+import net.minecraft.world.item.trading.MerchantOffers
+import org.bukkit.entity.Player
 
 @Serializable
 data class GuideBookPages(val pages: List<GuideBookPage> = listOf()) {
@@ -25,5 +40,23 @@ data class GuideBookPage(
     // The Serializer in PackyTemplates will always ensure the id is properly set
     @Transient val id: String = "",
     val title: String,
-    val buttons: List<SerializableItemStack>
-)
+    val buttons: List<SerializableItemStack> = emptyList()
+) {
+    fun openMerchantMenu(player: Player) {
+        //If no buttons, the page is only meant to change the title and doesnt have any sub-pages
+        if (buttons.isEmpty()) {
+            player.openInventory.title(title.miniMsg())
+        } else {
+            val serverPlayer = player.toNMS() as ServerPlayer
+            val clientMerchant = ClientSideMerchant(serverPlayer)
+            val merchantMenu = MerchantMenu(serverPlayer.nextContainerCounter(), serverPlayer.inventory, clientMerchant)
+            merchantMenu.setShowProgressBar(false)
+            val offers = MerchantOffers()
+            buttons.map(GuideBookHelpers::MerchantOffer).let(offers::addAll)
+            merchantMenu.offers = offers
+
+            clientMerchant.openTradingScreen(serverPlayer, PaperAdventure.asVanilla(title.miniMsg()), 0)
+        }
+
+    }
+}
