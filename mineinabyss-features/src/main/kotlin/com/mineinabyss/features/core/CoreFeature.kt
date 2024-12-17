efcd1e8d841cdd419f4c2815ac7e65593208bd4e
Boy <sivertpaulsen2@gmail.com>
2024-12-17 15:22:49 +0100

    feat: add serverlinks to core-feature

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/core/CoreFeature.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/core/CoreFeature.kt
@@ -3,7 +3,12 @@ package com.mineinabyss.features.core
 import com.mineinabyss.idofront.features.Feature
 import com.mineinabyss.idofront.features.FeatureDSL
 import com.mineinabyss.idofront.plugin.listeners
+import com.mineinabyss.idofront.textcomponents.miniMsg
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+import org.bukkit.Bukkit
+import java.net.URI
 
 class CoreFeature(val config: Config) : Feature() {
     @Serializable
@@ -13,10 +18,20 @@ class CoreFeature(val config: Config) : Feature() {
         val waterfallMoveMultiplier: Double = 0.15,
         val bubbleColumnDamageMultiplier: Double = 2.0,
         val bubbleColumnBreathMultiplier: Int = 2,
+        val serverLinks: List<ServerLink> = emptyList()
     )
+
+    @Serializable
+    data class ServerLink(@SerialName("displayName") private val _displayName: String, @SerialName("url") private val _url: String) {
+        @Transient val url = URI.create(_url)
+        @Transient val displayName = _displayName.miniMsg()
+    }
 
     override fun FeatureDSL.enable() {
 
         plugin.listeners(CoreListener(), PreventSignEditListener())
+        config.serverLinks.forEach {
+            Bukkit.getServerLinks().addLink(it.displayName, it.url)
+        }
     }
 }
