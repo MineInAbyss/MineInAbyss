34c562e46ec85a25982e57cbf42ed805e54e893a
Boy <sivertpaulsen2@gmail.com>
2025-02-10 15:17:56 +0100

    fix: apply waterfall/bubblecolumn logic for players in boats too

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/core/CoreListener.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/core/CoreListener.kt
@@ -9,9 +9,11 @@ import org.bukkit.block.Block
 import org.bukkit.block.data.Levelled
 import org.bukkit.block.data.type.BubbleColumn
 import org.bukkit.block.data.type.Light
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.event.vehicle.VehicleMoveEvent
 import kotlin.random.Random
 
 class CoreListener : Listener {
@@ -26,46 +28,68 @@ class CoreListener : Listener {
     }
 
     @EventHandler
+    fun VehicleMoveEvent.onMove() {
+        vehicle.passengers.filterIsInstance<Player>().forEach { player ->
+            if (player.gameMode.isInvulnerable) return@forEach
+
+            player.location.findLocationAround(radius = 1, scale = 0.30) {
+                it.block.isFlowing && it.up(4.0).block.isFlowing
+            }?.also { player.handleWaterfall() }
+
+            player.location.findLocationAround(radius = 1, scale = 0.30) {
+                it.clone().add(0.0, 4.0, 0.0).block.isBubbleColumn
+            }?.also { player.handleBubbleColumn() }
+
+            if (!player.isInWaterOrBubbleColumn) player.maximumAir = 300
+        }
+    }
+
+    @EventHandler
     fun PlayerMoveEvent.playerMove() {
         if (player.gameMode.isInvulnerable || !hasExplicitlyChangedBlock()) return
 
         player.location.findLocationAround(radius = 1, scale = 0.30) {
                 it.block.isFlowing && it.up(4.0).block.isFlowing
-        }?.let {
-            //bypass armor damage reduction
-            player.damage(0.0001) // trigger damage sound effect
-            player.health = (player.health - (0.25 * abyss.config.core.waterfallDamageMultiplier)).coerceAtLeast(0.0)
-
-            player.world.spawnParticle(Particle.CLOUD, player.location.add(0.0, 0.75, 0.0), 1, 0.5, 0.5, 0.5, 0.3)
-            player.velocity = player.velocity.apply {
-                x = Random.nextDouble(
-                    -abyss.config.core.waterfallMoveMultiplier,
-                    abyss.config.core.waterfallMoveMultiplier
-                )
-                y = -0.1
-                z = Random.nextDouble(
-                    -abyss.config.core.waterfallMoveMultiplier,
-                    abyss.config.core.waterfallMoveMultiplier
-                )
-            }
-        }
+        }?.also { player.handleWaterfall() }
 
         player.location.findLocationAround(radius = 1, scale = 0.30) {
             it.clone().add(0.0, 4.0, 0.0).block.isBubbleColumn
-        }?.let {
-            if (player.maximumAir <= 0) {
-                player.remainingAir = player.remainingAir
-                player.damage(0.0001) // trigger damage sound effect
-                player.health =
-                    (player.health - (0.25 * abyss.config.core.bubbleColumnDamageMultiplier)).coerceAtLeast(0.0)
+        }?.also { player.handleBubbleColumn() }
 
-            } else {
-                player.remainingAir = (player.maximumAir - abyss.config.core.bubbleColumnBreathMultiplier)
-                player.maximumAir = player.remainingAir.coerceAtLeast(0)
-            }
-
-        }
         if (!player.isInWaterOrBubbleColumn) player.maximumAir = 300
+    }
+
+    private fun Player.handleWaterfall() {
+        //bypass armor damage reduction
+        damage(0.0001) // trigger damage sound effect
+        health = (health - (0.25 * abyss.config.core.waterfallDamageMultiplier)).coerceAtLeast(0.0)
+
+        world.spawnParticle(Particle.CLOUD, location.add(0.0, 0.75, 0.0), 1, 0.5, 0.5, 0.5, 0.3)
+        velocity = velocity.apply {
+            x = Random.nextDouble(
+                -abyss.config.core.waterfallMoveMultiplier,
+                abyss.config.core.waterfallMoveMultiplier
+            )
+            y = -0.1
+            z = Random.nextDouble(
+                -abyss.config.core.waterfallMoveMultiplier,
+                abyss.config.core.waterfallMoveMultiplier
+            )
+        }
+    }
+
+    private fun Player.handleBubbleColumn() {
+        if (maximumAir <= 0) {
+            remainingAir = remainingAir
+            damage(0.0001) // trigger damage sound effect
+            health =
+                (health - (0.25 * abyss.config.core.bubbleColumnDamageMultiplier)).coerceAtLeast(0.0)
+
+        } else {
+            remainingAir = (maximumAir - abyss.config.core.bubbleColumnBreathMultiplier)
+            maximumAir = remainingAir.coerceAtLeast(0)
+        }
+
     }
 
     private val Block.isFlowing: Boolean get() = ((blockData as? Levelled)?.level ?: 0) >= 8 && blockData !is Light
