8b1aa42c101db26d7e078efef883a5a3c1d68c04
user <soulhood2.0@gmail.com>
2025-07-22 23:52:44 +0200

    feat: component for noaccessmessage

--- mineinabyss-components/src/main/kotlin/com/mineinabyss/components/gondolas/Gondola.kt
+++ mineinabyss-components/src/main/kotlin/com/mineinabyss/components/gondolas/Gondola.kt
@@ -1,9 +1,13 @@
 package com.mineinabyss.components.gondolas
 
+
 import com.mineinabyss.idofront.serialization.LocationSerializer
 import com.mineinabyss.idofront.serialization.SerializableItemStack
+import com.mineinabyss.idofront.textcomponents.miniMsg
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+import net.kyori.adventure.text.Component
 import org.bukkit.Location
 
 @Serializable
@@ -17,7 +21,12 @@ class Gondola(
     val displayItem: SerializableItemStack, // the item to display in the GUI
     val displayName: String,
     val warpZoneRange: Double, // the range in which the player needs to stay in order to be teleported
-    val noAccessMessage: String, // the message to display when the player tries to use the gondola without perms
+    @SerialName("no_access_message")
+    private val _noAccessMessage: String, // the message to display when the player tries to use the gondola without perms
+    val warpCooldown: Long = 5000
 ) {
-    val warpCooldown = 5000
+    @Transient
+    val parsedNoAccessMessage: Component get() = _noAccessMessage.miniMsg()
+    @Transient
+    val rawNoAccessMessage: String get() = _noAccessMessage
 }
