4069f62a47c3a0e5f4886834d437ce739007bc97
Helia8 <126818312+Helia8@users.noreply.github.com>
2025-07-23 18:13:44 +0200

    feat: Implement Gondola System (#205)
    * fix: async teleport
    
    * feat: added displayName and id
    
    * feat: gondola system
    
    * refactor: made the gondola listener easier to read
    
    * refactor: used associatebyto instead of a cast when assigning the loaded gondolas
    
    * refactor: stopped using AABB detection and instead use bukkit's builtin
    
    * feat: mapped gondolas to ids
    
    * feat: component for noaccessmessage
    
    * feat: tab completion for gondolas commands
    
    * refactor: changed loaded gondolas to val
    
    * refactor: used PlayerMoveEvent.onPlayerMove() to improve readability
    
    * refactor: cleanup
    
    * refactor: cleanup
    
    * refactor: code style && used GondolaData class to store data

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/gondolas/GondolaGUI.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/gondolas/GondolaGUI.kt
@@ -7,12 +7,13 @@ import com.mineinabyss.geary.papermc.tracking.entities.toGeary
 import com.mineinabyss.guiy.components.HorizontalGrid
 import com.mineinabyss.guiy.components.Item
 import com.mineinabyss.guiy.components.canvases.Chest
+import com.mineinabyss.guiy.inventory.GuiyOwner
 import com.mineinabyss.guiy.inventory.LocalGuiyOwner
 import com.mineinabyss.guiy.modifiers.Modifier
 import com.mineinabyss.guiy.modifiers.click.clickable
 import com.mineinabyss.guiy.modifiers.size
+import com.mineinabyss.idofront.items.editItemMeta
 import com.mineinabyss.idofront.textcomponents.miniMsg
-import io.papermc.paper.datacomponent.DataComponentTypes
 import org.bukkit.entity.Player
 
 @Composable
@@ -21,17 +22,23 @@ fun GondolaSelectionMenu(player: Player) {
     val gearyPlayer = player.toGeary()
     val gondolas = gearyPlayer.get<UnlockedGondolas>() ?: return
 
-    Chest(setOf(player), title = "Choose Spawn Location", onClose = { owner.exit() }) {
+    Chest(
+        setOf(player),
+        title = "Choose Spawn Location",
+        onClose = { owner.exit() }) {
         HorizontalGrid(Modifier.size(9, 6)) {
-            gondolas.keys.forEach { GondolaSpawn(player, LoadedGondolas.loaded[it] ?: return@forEach) }
+            gondolas.keys.forEach {
+                GondolaSpawn(player,LoadedGondolas.loaded[it] ?: return@forEach)
+            }
         }
     }
 }
 
 @Composable
 fun GondolaSpawn(player: Player, gondola: Gondola) = Item(
-    gondola.displayItem.toItemStack().apply { setData(DataComponentTypes.ITEM_NAME, gondola.name.miniMsg()) },
+    gondola.displayItem.toItemStack()
+        .editItemMeta { itemName(gondola.displayName.miniMsg()) },
     Modifier.clickable {
-        player.teleport(gondola.location)
+        player.teleportAsync(gondola.upperLoc)
     }
 )
