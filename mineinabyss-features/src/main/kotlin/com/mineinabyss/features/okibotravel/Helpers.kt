101c43a3f32ff354a3249b2f90abe0d05a879be2
Boy0000 <sivertpaulsen2@gmail.com>
2025-08-07 23:03:13 +0200

    fix: okiboline facing wrong direction for "forward" stations

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/okibotravel/Helpers.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/okibotravel/Helpers.kt
@@ -1,16 +1,20 @@
 package com.mineinabyss.features.okibotravel
 
+import com.bergerkiller.bukkit.coasters.TCCoasters
+import com.bergerkiller.bukkit.coasters.tracks.TrackNodeSearchPath
 import com.bergerkiller.bukkit.tc.TrainCarts
 import com.bergerkiller.bukkit.tc.controller.spawnable.SpawnableGroup
 import com.bergerkiller.bukkit.tc.properties.standard.type.CollisionOptions
 import com.mineinabyss.components.okibotravel.OkiboLineStation
 import com.mineinabyss.features.abyss
 import com.mineinabyss.features.helpers.di.Features.okiboLine
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
+import org.bukkit.util.Vector
 import kotlin.math.roundToInt
 
 internal fun spawnOkiboCart(player: Player, station: OkiboLineStation, destination: OkiboLineStation) {
-    val direction = station.nextStation(destination)?.location?.toVector()?.subtract(station.location.toVector())?.normalize() ?: station.location.direction
+    val direction = station.direction(destination)
     val spawnGroup = SpawnableGroup.parse(TrainCarts.plugin, "OkiboCartPaid")
     val spawnLocations = spawnGroup.findSpawnLocations(station.location, direction, SpawnableGroup.SpawnMode.DEFAULT)
     val train = spawnGroup.spawn(spawnLocations)
@@ -24,6 +28,7 @@ internal fun spawnOkiboCart(player: Player, station: OkiboLineStation, destinati
     train.properties.isSlowingDown = false
     train.properties.collision = CollisionOptions.CANCEL
     train.properties.isPlayerTakeable = false
+    train.properties.canOnlyOwnersEnter = true
     train.properties.isManualMovementAllowed = true
 
     abyss.logger.i("A train has been spawned at ${station.id} and is heading to ${destination.id}!")
@@ -37,12 +42,12 @@ fun OkiboLineStation.costTo(destination: OkiboLineStation): Int? {
     return (startNode.findConnection(destNode)?.distance?.times(okiboLine.config.costPerKM)?.div(1000))?.roundToInt()
 }
 
-val OkiboLineStation.isSubStation get() = okiboLine.config.okiboStations.any { this in it.subStations }
-val OkiboLineStation.parentStation get() = okiboLine.config.okiboStations.firstOrNull { this in it.subStations }
-fun OkiboLineStation.nextStation(destination: OkiboLineStation) : OkiboLineStation? {
-    TrainCarts.plugin.pathProvider.getWorld(location.world).let {
-        val (startNode, endNode) = (it.getNodeByName(id) ?: return null) to (it.getNodeByName(destination.id) ?: return null)
-        val nextStation = startNode.findRoute(endNode).first().destination.name
-        return okiboLine.config.allStations.find { it.id == nextStation }
-    }
+val tccoasters by lazy { Bukkit.getPluginManager().getPlugin("TCCoasters") as TCCoasters }
+fun OkiboLineStation.direction(destination: OkiboLineStation) : Vector {
+    val tccWorld = tccoasters.getCoasterWorld(location.world)
+    val startNode = tccWorld.rails.findAtBlock(location.block).values().find { it.node().signs.isNotEmpty() }?.node()!!
+    val destNode = tccWorld.rails.findAtBlock(destination.location.block).values().find { it.node().signs.isNotEmpty() }?.node()!!
+    val nextNode = TrackNodeSearchPath.findShortest(startNode, mutableSetOf(destNode)).pathConnections.first()
+
+    return nextNode.getDirection(startNode)
 }
