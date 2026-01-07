c3358b4107fd8c75c1b7f475955750d591e31e1a
Boy0000 <sivertpaulsen2@gmail.com>
2026-01-07 14:23:09 +0100

    chore: make LocationData ranges transient

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/quests/LocationsConfig.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/quests/LocationsConfig.kt
@@ -1,8 +1,8 @@
 package com.mineinabyss.features.quests
 
 import com.mineinabyss.idofront.serialization.LocationAltSerializer
-import com.mineinabyss.idofront.serialization.LocationSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import org.bukkit.Location
 
 // List of all the locations that are to be detected by quests
@@ -15,15 +15,12 @@ class LocationData(
     val radius: Int,
 ) {
     //    var visited : Boolean = false
-    val minX = locationCenter.blockX - radius
-    val maxX = locationCenter.blockX + radius
-    val minY = locationCenter.blockY - radius
-    val maxY = locationCenter.blockY + radius
-    val minZ = locationCenter.blockZ - radius
-    val maxZ = locationCenter.blockZ + radius
+    @Transient val xRange = (locationCenter.blockX - radius)..(locationCenter.blockX + radius)
+    @Transient val yRange = (locationCenter.blockY - radius)..(locationCenter.blockY + radius)
+    @Transient val zRange = (locationCenter.blockZ - radius)..(locationCenter.blockZ + radius)
 
     fun isInside(location: Location, flat: Boolean = false): Boolean {
-        return location.blockX in minX..maxX && location.blockZ in minZ..maxZ && (flat || location.blockY in minY..maxY)
+        return location.blockX in xRange && location.blockZ in zRange && (flat || location.blockY in yRange)
     }
 
 }
